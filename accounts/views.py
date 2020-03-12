7fb287987e6572763e50831cd7d3812601ae8572
chokniran chongsomchit <chokniran@gmail.com>
2020-03-12 22:40:09 +0700

    create template login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 
 # Create your views here.
 def login(request):
-    return render(request, 'account/login.html')
+ return render(request, 'account/login.html')
+
+    
+
